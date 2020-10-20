7479162b3fe0ef3817ecf44868e4f22e4189d653
rajadubey1997 <rajadubey1997@gmail.com>
2020-10-20 19:22:56 +0530

    visual changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,16 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    render() {
-
-        return(
-           <>
-//write your code here
-	</>
+    relatives = ["Hitesh", "Sanya", "Nilu", "Snehil", "Nisha"];
+  render() {
+    return (
+      <>
+        <ol key="realtiveList">
+          {this.relatives.map((rel, index) => (
+            <li key={`relativeListItem${index + 1}`}>{rel}</li>
+          ))}
+        </ol>
+      </>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 function generateCodeFromObject(obj){
     //return a code generated string
+    const relatives = ["Hitesh", "Sanya", "Nilu", "Snehil", "Nisha"];
+
+      return (
+        <>
+        <ol key="realtiveList">
+        {this.relatives.map((relative, index) => (
+          <li key={"relativeListItem" + (index + 1)}>{relative}</li>
+        ))}
+      </ol>
+    </>
+  );
    }
    
 module.exports=generateCodeFromObject;
