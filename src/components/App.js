8a120bd14d69bf877bd077ea77f342141f53d95a
rajadubey1997 <rajadubey1997@gmail.com>
2020-10-21 14:26:16 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,7 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    relatives = ["Hitesh", "Sanya", "Nilu", "Snehil", "Nisha"];
-  render() {
-    return (
-      <>
-        <ol key="realtiveList">
-          {this.relatives.map((rel, index) => (
-            <li key={`relativeListItem${index + 1}`}>{rel}</li>
-          ))}
-        </ol>
-      </>
-        )
-    }
+    
 }
 
 
