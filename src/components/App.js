2cc26e568cab282da6e1194587eecc8a9cead348
Raja Babu Dubey <45536846+rajadubey@users.noreply.github.com>
2020-10-21 18:32:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    
+   
+   render() {
+      let relativeList = ["Vikram", "Aditya", "Raja", "Rahul", "Priya"];
+      return (
+         <>
+            <ol key={"relativeList"}>
+               {relativeList.map((relative, i) => (
+                  <li key={`relativeListItem${i + 1}`}>{relative}</li>
+               ))}
+            </ol>
+         </>
+      );
+   }
 }
-
 
 export default App;
